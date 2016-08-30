8c4e1d19999bdb89820d976af426dee0eacf9aff
Nahuel Defossé <nahuel.defosse@gmail.com>
2016-08-30 15:48:25 -0300

    Prevent error in console

--- trackstats/static/trackstats/js/graphs.js
+++ trackstats/static/trackstats/js/graphs.js
@@ -1,16 +1,23 @@
 (function () {
+    "use strict";
 
     function init() {
         google.load('visualization', '1', {packages: ['corechart']});
         google.setOnLoadCallback(drawChart);
 
         function drawChart() {
+            var dataElement = document.getElementById('trackstats-graph');
+            if (dataElement === null) {
+                console.info("No data for plotting");
+                return;
+            }
+
             var data = new google.visualization.DataTable();
             data.addColumn('date', 'Date');
             data.addColumn('number', 'Value');
 
             data.addRows(graphData);
-            var chart = new google.visualization.ColumnChart(document.getElementById('trackstats-graph'));
+            var chart = new google.visualization.ColumnChart(dataElement);
             chart.draw(data, graphOptions);
         }
     }
